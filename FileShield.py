00ac99c268c2d1e2b632b02a657942a3d9071a13
Mete Vesek <metevesekk@hotmail.com>
2024-08-01 22:51:46 +0300

    button updates

--- FileShield.py
+++ FileShield.py
@@ -112,11 +112,12 @@ def decrypt_file_action():
 # GUI Tasarımı
 root = tk.Tk()
 root.title("FileShield")
-root.geometry("500x400")
+root.geometry("555x370")
 root.configure(bg="#F5F5F5")
+root.resizable(False, False)  # Pencereyi yeniden boyutlandırmayı kapatır
 
 # Başlık
-title_label = tk.Label(root, text="FileShield", font=("Helvetica Neue", 24, "bold"), fg="black", bg="#F5F5F5")
+title_label = tk.Label(root, text="FileShield", font=("Orbitron", 24, "bold"), fg="black", bg="#F5F5F5")
 title_label.pack(pady=(10, 5))
 
 # Logo Container
@@ -124,7 +125,7 @@ logo_frame = tk.Frame(root, bg="#F5F5F5")
 logo_frame.pack(pady=(0, 10))
 # Logo ekle
 logo_image = Image.open("/Users/metevesek/Desktop/FileShield/icons/appIcon.png")
-logo_image = logo_image.resize((80, 80), Image.Resampling.LANCZOS)
+logo_image = logo_image.resize((60, 60), Image.Resampling.LANCZOS)  # Logo boyutu küçültüldü
 logo_photo = ImageTk.PhotoImage(logo_image)
 logo_label = tk.Label(logo_frame, image=logo_photo, bg="#F5F5F5")
 logo_label.pack()
@@ -134,40 +135,40 @@ form_frame = tk.Frame(root, bg="#F5F5F5")
 form_frame.pack(pady=10, fill="x", padx=20)
 
 # Dosya Seçim Butonu
-lbl_file = tk.Label(form_frame, text="Select File:", font=("Helvetica Neue", 12), bg="#F5F5F5")
+lbl_file = tk.Label(form_frame, text="Select File:", font=("Orbitron", 12, "bold"), bg="#F5F5F5")
 lbl_file.grid(row=0, column=0, padx=10, pady=5, sticky="w")
-entry_file_path = tk.Entry(form_frame, width=30, font=("Helvetica Neue", 12))
+entry_file_path = tk.Entry(form_frame, width=30, font=("Orbitron", 12))
 entry_file_path.grid(row=0, column=1, padx=10, pady=5, sticky="w")
-btn_select_file = tk.Button(form_frame, text="Browse", command=select_file, font=("Helvetica Neue", 10), bg="#007AFF", fg="black")
+btn_select_file = tk.Button(form_frame, text="Browse", command=select_file, font=("Orbitron", 12, "bold"), bg="#007AFF", fg="black", width=80, compound=tk.RIGHT)
 btn_select_file.grid(row=0, column=2, padx=10, pady=5)
 
 # Parola ve Onay Parolası
-lbl_password = tk.Label(form_frame, text="Password:", font=("Helvetica Neue", 12), bg="#F5F5F5")
+lbl_password = tk.Label(form_frame, text="Password:", font=("Orbitron", 12, "bold"), bg="#F5F5F5")
 lbl_password.grid(row=1, column=0, padx=10, pady=5, sticky="w")
-entry_password = tk.Entry(form_frame, show="*", width=30, font=("Helvetica Neue", 12))
+entry_password = tk.Entry(form_frame, show="*", width=30, font=("Orbitron", 12))
 entry_password.grid(row=1, column=1, padx=10, pady=5, sticky="w")
 
-lbl_confirm_password = tk.Label(form_frame, text="Confirm Password:", font=("Helvetica Neue", 12), bg="#F5F5F5")
+lbl_confirm_password = tk.Label(form_frame, text="Confirm Password:", font=("Orbitron", 12, "bold"), bg="#F5F5F5")
 lbl_confirm_password.grid(row=2, column=0, padx=10, pady=5, sticky="w")
-entry_confirm_password = tk.Entry(form_frame, show="*", width=30, font=("Helvetica Neue", 12))
+entry_confirm_password = tk.Entry(form_frame, show="*", width=30, font=("Orbitron", 12))
 entry_confirm_password.grid(row=2, column=1, padx=10, pady=5, sticky="w")
 
 # Algoritma Seçimi
-lbl_algo = tk.Label(form_frame, text="Algorithm:", font=("Helvetica Neue", 12), bg="#F5F5F5")
+lbl_algo = tk.Label(form_frame, text="Algorithm:", font=("Orbitron", 12, "bold"), bg="#F5F5F5")
 lbl_algo.grid(row=3, column=0, padx=10, pady=5, sticky="w")
 algo_var = tk.StringVar(value='Fernet')
-algo_combo = ttk.Combobox(form_frame, textvariable=algo_var, values=['Fernet', 'AES', 'DES', 'Blowfish'], font=("Helvetica Neue", 12))
+algo_combo = ttk.Combobox(form_frame, textvariable=algo_var, values=['Fernet', 'AES', 'DES', 'Blowfish'], font=("Orbitron", 12))
 algo_combo.grid(row=3, column=1, padx=10, pady=5, sticky="w")
 
 # Encrypt ve Decrypt Butonları
 frame_buttons = tk.Frame(root, bg="#F5F5F5")
 frame_buttons.pack(pady=20, fill="x", padx=20)
-btn_encrypt = tk.Button(frame_buttons, text="Encrypt", command=protect_file, font=("Helvetica Neue", 12, "bold"), bg="#34C759", fg="black", width=80, compound=tk.RIGHT)
-btn_encrypt.pack(side="left", padx=10, pady=(0, 5))
-btn_decrypt = tk.Button(frame_buttons, text="Decrypt", command=decrypt_file_action, font=("Helvetica Neue", 12, "bold"), bg="#FF3B30", fg="black", width=80, compound=tk.RIGHT)
-btn_decrypt.pack(side="right", padx=10, pady=(0, 5))
+btn_encrypt = tk.Button(frame_buttons, text="Encrypt", command=protect_file, font=("Orbitron", 12, "bold"), bg="#34C759", fg="black", width=80, compound=tk.RIGHT)
+btn_encrypt.pack(side="left", padx=5, pady=(0, 5))
+btn_decrypt = tk.Button(frame_buttons, text="Decrypt", command=decrypt_file_action, font=("Orbitron", 12, "bold"), bg="#FF3B30", fg="black", width=80, compound=tk.RIGHT)
+btn_decrypt.pack(side="left", padx=5, pady=(0, 5))
 
-# Encrypt ve Decrypt butonlarına resim ekleme
+# Encrypt, Decrypt ve Browse butonlarına resim ekleme
 encrypt_image = Image.open("/Users/metevesek/Desktop/FileShield/icons/encryptIcon.png")
 encrypt_image = encrypt_image.resize((20, 20), Image.Resampling.LANCZOS)
 encrypt_photo = ImageTk.PhotoImage(encrypt_image)
@@ -177,5 +178,10 @@ decrypt_image = Image.open("/Users/metevesek/Desktop/FileShield/icons/decryptIco
 decrypt_image = decrypt_image.resize((20, 20), Image.Resampling.LANCZOS)
 decrypt_photo = ImageTk.PhotoImage(decrypt_image)
 btn_decrypt.config(image=decrypt_photo, compound="right")
+
+browse_image = Image.open("/Users/metevesek/Desktop/FileShield/icons/browseIcon.png")
+browse_image = browse_image.resize((20, 20), Image.Resampling.LANCZOS)
+browse_photo = ImageTk.PhotoImage(browse_image)
+btn_select_file.config(image=browse_photo, compound="right")
 
 root.mainloop()
